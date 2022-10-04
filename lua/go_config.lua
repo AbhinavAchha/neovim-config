27e11405540a247a23f8983d66e790d2b234bd11
Abhinav Achha <achhaabhinav@gmail.com>
2022-10-04 19:15:07 +0530

    add some plugins

--- lua/go_config.lua
+++ lua/go_config.lua
@@ -2,7 +2,7 @@ require("go").setup({
 	disable_defaults = false, -- true|false when true set false to all boolean settings and replace all table
 	-- settings with {}
 	go = "go", -- go command, can be go[default] or go1.18beta1
-	goimport = "gopls", -- goimport command, can be gopls[default] or goimport
+	goimport = "golines", -- goimport command, can be gopls[default] or goimport
 	fillstruct = "gopls", -- can be nil (use fillstruct, slower) and gopls
 	gofmt = "gofumpt", --gofmt cmd,
 	max_line_len = 100, -- max line length in golines format, Target maximum line length for golines
